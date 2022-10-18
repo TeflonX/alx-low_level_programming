06a059dcde03dbf2797bc2dde83e0af57414c93e
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-17 19:22:29 -0700

    small letter alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = getchar;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
