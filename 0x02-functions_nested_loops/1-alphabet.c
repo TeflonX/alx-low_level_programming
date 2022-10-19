e97aa1d71097a13b8680bc378e62e0522135a642
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-19 02:37:32 -0700

    Function that prints small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - block of code that print small letters
+ *
+ * Return: nothing (Success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
