4f352cc9116fc51f94ce448b75e30985b1af1410
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-18 21:58:06 -0700

    First _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 95;
+
+	char p = 'p';
+
+	char u = 'u';
+
+	char t = 't';
+
+	char c = 'c';
+
+	char h = 'h';
+
+	char a = 'a';
+
+	char r = 'r';
+
+	_putchar(i);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
+
+	return (0);
+}
