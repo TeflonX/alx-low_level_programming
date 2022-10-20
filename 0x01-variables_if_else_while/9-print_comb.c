51695d03b5b45506b8f8ff85e90c9edd1e5036ba
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-20 00:04:49 -0700

    Print possible combination of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 10; i <= 19; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if (i / 10)
-		{
-			putchar(' ');
-			putchar(i % 10 + '0');
-			putchar(',');
-		}
+		putchar(i);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
