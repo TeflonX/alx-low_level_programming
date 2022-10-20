5736796853c68e9418709ad891c6440e1e2341c2
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-20 01:51:15 -0700

    Print different combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+	
+	int j;
+
+	int k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (i >= j || i >= k || j >= k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if ( i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
