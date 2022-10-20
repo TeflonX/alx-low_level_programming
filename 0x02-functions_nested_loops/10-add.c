4a86d68f503db9b5c4fbe990ed5e0b6c36d17bd1
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-19 18:27:04 -0700

    Function that adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that performs addition.
+ *
+ * Return: a + b;
+ *
+ * @a: integer
+ *
+ * @b: integer parameter
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
