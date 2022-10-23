4d43f33ff7414544619daea7d6d74bd0fddbb4a5
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-22 20:51:00 -0700

    Print last digit

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,6 +2,16 @@
 
 int main(void)
 {
-	times_table();
-	return (0);
+	int r;
+
+	 print_last_digit(98);
+
+	     print_last_digit(0);
+
+	         r = print_last_digit(-1024);
+	 _putchar('0' + r);
+
+	     _putchar('\n');
+
+	         return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,14 @@
  */
 
 int print_last_digit(int n)
-{
-	return (n % 10);
+{	
+	int lastdig;
+
+	lastdig = n % 10;
+	if (lastdig < 0)
+	{
+		lastdig = lastdig * -1;
+	}
+	_putchar(lastdig + '0');
+	return (lastdig);
 }
