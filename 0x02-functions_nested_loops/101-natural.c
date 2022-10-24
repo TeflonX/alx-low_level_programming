a51ebe4f2b6037fe316119fc11a5584bdcba8f8a
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-23 21:38:51 -0700

    Multiple of 3 or 5 addition

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- *  main - prints the sum of multiples.
+ * main - prints the sum of multiples.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int n, i, a;
 
-	a = 15;
+	int i;
 
-	for (i = 1; i <= 66; i = i++)
+	int n = 0;
+
+	for (i = 1; i <= 1023; i = i++)
 	{
-		n = a + 15;
-		a = n;
-		if (i == 66)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
+			n += i;
 			printf("%d", n);
 		}
 	}
