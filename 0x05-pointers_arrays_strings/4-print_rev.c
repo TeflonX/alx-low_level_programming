162843b697478e52488272e2605953097c009ae6
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-24 21:33:16 -0700

    Prints every other character in a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (s[i] != '\0'; i = 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,5 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
