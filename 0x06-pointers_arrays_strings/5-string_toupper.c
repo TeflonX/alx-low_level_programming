3243f831e07ddff33cc783dd8d6f095c42d1c460
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-10-27 21:40:53 -0700

    Uppercase to lowercase function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase.
+ * Return: char.
+ * @str: pointer.
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+	
+	char str;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return(str)
+}
+	
