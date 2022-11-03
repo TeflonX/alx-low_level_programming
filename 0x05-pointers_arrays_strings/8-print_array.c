c03c3828c3e8459da3b5c373f58272e914b1b62c
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-02 20:20:59 -0700

    prints n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array.
+ * Return: void.
+ * @a: array pointer.
+ * @n: integer argument.
+ */
+
+void print_array(int *a, int n)
+{
+	for (int i = 0; i <= n - 1; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d ,", a[i]);
+		}
+		else
+		{
+		printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
