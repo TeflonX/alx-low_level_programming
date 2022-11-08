302dc724144f2e5b41c11937c546fc89cc7def10
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-08 14:12:09 -0800

    copied both string into the memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,42 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+/**
+ * str_concat - copies two string into a memory location.
+ * Return: char pointer to the location.
+ * @s1: pointer to the first string.
+ * @s2: pointer to the second string.
+ */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1 = len2 = 0;
+	int i, j, len1, len2;
 
 	char *s;
 
+	len1 = len2 = 0;
 
 	if (s1 != NULL)
 		len1 = strlen(s1);
-	
+
 	if (s2 != NULL)
 		len2 = strlen(s2);
-	
+
 	s = malloc((len1 + len2 + 1) * sizeof(char));
 
-	
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	
-	for (i = 0; i <= len2; i++)
+
+	for (i = 0; i <= len1; i++)
 	{
-		s1[len1 + i] = s1[i];
+		s[i] = s1[i];
 	}
 
-	for (j = 0; j <= (len1 + len2 + 1); j++)
+	for (j = 0; j <= len2; j++)
 	{
-		s[j] = s1[j];
+		s[len1 + j] = s2[j];
 	}
 	return (s);
 }
