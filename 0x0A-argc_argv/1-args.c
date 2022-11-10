cdc5abf12b3959e6bce733f0c1479d8fce483d59
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-09 16:17:40 -0800

    prints out argument  count on terminal

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguent passed.
+ * Return: 0 (Success)
+ * @argc: argument count.
+ * @argv: argument vector.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("Number of argument passed: %d\n", argc);
+
+	return (0);
+}
