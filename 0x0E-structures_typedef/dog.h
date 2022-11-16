53197eec211a4ce0c91e08ac2f79940683d7f772
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-15 18:48:07 -0800

    added a typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,11 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
 #endif
