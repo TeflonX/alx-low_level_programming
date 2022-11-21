8c0085c8e60a4766b88e37d99b54395ad871ecd1
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 03:46:12 -0800

    added condition for three digit numbers

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,7 +2,20 @@
 
 int main(void)
 {
-	
-	times_table();
+	 print_times_table(3);
+
+	     _putchar('\n');
+
+	         print_times_table(5);
+
+		     _putchar('\n');
+
+		         print_times_table(98);
+
+			     _putchar('\n');
+
+			         print_times_table(12);  
+
+				     return (0);
 	return (0);
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,51 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * print_times_table -  a function that prints the n times table,
+ * starting with 0.
+ * Return: void (Success).
+ * @n: integer argument.
+ */
 
 void print_times_table(int n)
 {
 	int i, j, o;
 
-	for (i = '0'; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = '0'; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			o = i * j;
-			_putchar(o);
-			
-			if (j != n)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				o = i * j;
+				if (j == 0)
+					_putchar(o + '0');
+				if ((j >= 1 && j <= n) && o < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(o + '0');
+				}
+				if ((j >= 1 && j <= n) && (o >= 10 && o <=
+							99))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((o / 10) + '0');
+					_putchar((o % 10) + '0');
+				}
+				if ((j >= 1 && j <= n) && o >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((o / 100) + '0');
+					_putchar(((o / 10) % 10) + '0');
+					_putchar((o % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
