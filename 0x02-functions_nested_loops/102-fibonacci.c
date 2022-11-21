31a1b76f1e9447194356fc5c78bce5b9fef0f7f0
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 06:34:24 -0800

    used unsigned int data type

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-
+/**
+ * main - a program that prints the first 50 Fibonacci numbers, starting
+ * with 1 and 2.
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	int i;
+	unsigned long int i, t1, t2, nextTerm;
 
-	int t1 = 1,t2 = 2;
+	t1 = 1;
+	t2 = 2;
+	nextTerm = t1 + t2;
+	printf("%lu, %lu, %lu, ", t1, t2, nextTerm);
 
-	unsigned long long int nextTerm = t1 + t2;
-
-	for (i = 5; i <= 50; ++i)
+	for (i = 1; i <= 47; ++i)
 	{
-		if (i == 50)
-		{
-			printf("%llu", nextTerm);
-		}
-		else
-		{
-			printf("%llu, ", nextTerm);
-		}
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
+
+		if ((nextTerm >= 1) && (nextTerm <= 2147483647))
+		{
+			printf("%lu", nextTerm);
+		}
+		if (nextTerm > 2147483647)
+		{
+			printf("%lu", (nextTerm / 10));
+			printf("%lu", (nextTerm % 10));
+		}
+		if (i != 47)
+			printf(", ");
 	}
 	printf("\n");
 
