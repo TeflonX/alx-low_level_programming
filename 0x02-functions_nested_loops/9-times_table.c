6414abd0cdd848d9ed9fee05828658fb76d3adf3
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 02:39:41 -0800

    modified the if condition to get the first column

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * times_table - a function that prints the nine times table.
+ * Return: void (Success)
+ */
 void times_table(void)
 {
 	int i, j, o;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			o = (i - '0') * (j - '0');
-			_putchar(o + '0');
-			
-			if (j != '9')
+			o = i * j;
+			if (j == 0)
+				_putchar(o + '0');
+			if ((j >= 1 && j <= 9) && o < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(o + '0');
+
+			}
+			if ((j >= 1 && j <= 9) && o >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((o / 10) + '0');
+				_putchar((o % 10) + '0');
 			}
 		}
 		_putchar('\n');
