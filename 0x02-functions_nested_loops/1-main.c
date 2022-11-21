3855ea900e03630752a45734c9208304a9613ba4
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 01:00:48 -0800

    reduced the number of for loop

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-	times_table();
+	
+	 jack_bauer();
 	return (0);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,27 +9,34 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 48; i <= 50; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j = 0; j < 60; j++)
 		{
-			for (k = 48; k <= 53; k++)
+			if (i < 10)
 			{
-				for (l = 48; k <= 57; l++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					_putchar('\n');
-				}
+				_putchar('0');
+				_putchar(i + '0');
 			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		_putchar('\n');
 		}
-		if (i == 50 && j == 51 && k == 53 && l == 57)
-			break;
 	}
 }
