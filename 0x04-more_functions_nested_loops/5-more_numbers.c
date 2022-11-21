ba7326451d7e8b374a2bd5ba13c44f0e4bfc97a3
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 08:22:59 -0800

    modified value of j with %

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,21 @@
 #include "main.h"
-
-
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14.
+ * Return: void (Success)
+ */
 void more_numbers(void)
 {
 	int i;
+
 	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(i);
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
