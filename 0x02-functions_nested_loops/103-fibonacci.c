08f4681624fdefcd78ea9b5754492d60e16b8390
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-21 05:17:57 -0800

    added the variable, sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+/**
+ * main - function that prints sum of even valued term of fibonacci sequence.
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-	int i;
+	int i, n, t1, t2, nextTerm, sum;
 
-	int t1 = 1,t2 = 2;
-
-	unsigned long long int nextTerm = t1 + t2;
+	t1 = 1;
+	t2 = 2;
+	nextTerm = t1 + t2;
+	sum = 0;
 
 	for (i = 3; i <= 50; ++i)
 	{
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
-		if (nextTerm > 4000000)
-			break;
-		if (nextTerm % 2 == 0)
-			printf("%d, ", nextTerm);	
+
+		if ((nextTerm >= 1) && (nextTerm <= 4000000))
+		{
+			if (nextTerm % 2 == 0)
+			{
+				sum = sum + nextTerm;
+			}
+		}
 	}
-	printf("\n");
+
+	n = sum + 2;
+	printf("%d\n", n);
 
 	return (0);
 }
