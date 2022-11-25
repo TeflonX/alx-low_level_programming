3badb0962bab5106aee40e0bc497a65eedb13665
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-25 04:52:29 -0800

    initialized value of count as one and added a print function for when next is null

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * Return: number of nodes in singly linked list.
+ * @h: pointer to the head of list.
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+
+	size_t count = 1;
+
+	temp = h;
+
+	if ((temp->str) == NULL)
+		printf("[0] (nil)");
+
+	while ((temp->next) != NULL)
+	{
+		printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		count++;
+	}
+
+	printf("[%d] %s\n", temp->len, temp->str);
+
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+My project on Singly linked lists.
