4e2e667d3c94bfa1faf07209a8881f0be5cbdc1c
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-25 05:14:01 -0800

    added <stddef.h> to the header file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - a function that returns the number of elements in a linked
+ * list.
+ * Return: count, number of elements in the linked list.
+ * @h: head of the list.
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+
+	size_t count = 1;
+
+	temp = h;
+
+	while ((temp->next) != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	return (count);
+}
