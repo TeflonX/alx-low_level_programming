f6b4ddd8c60bbf73d19514d3c0f91d8a31050c51
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-25 07:16:54 -0800

    modified the pointer to the head and added strlen

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * add_node -  a function that adds a new node at the beginning of a
+ * linked list.
+ * Return: pointer to the new node inserted.
+ * @head: double pointer to the new node.
+ * @str: string added to the node.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
-	
+
 	list_t *size;
 
 	size = malloc(sizeof(list_t));
