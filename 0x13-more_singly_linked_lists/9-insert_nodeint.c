b12e10f8c644eacb9beec3950b85c7e06be2a6d0
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-11-28 23:54:45 -0800

    creates and inserts a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,32 +1,53 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * insert_nodeint_at_index - a function that inserts a new node at
+ * a given position.
+ * Return: *head (pointer to the head node)
+ * @head: double pointer to the head node.
+ * @idx: index of the new node.
+ * @n: integer component of the new node.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head,
 unsigned int idx, int n)
 {
-	listint_t *trav;
+	listint_t *trav, *temp, *edge, *stag;
 
-	listint_t *temp;
-
-	unsigned int i;
-
-	if (!*head)
-		return (NULL);
+	unsigned int i, count;
 
 	temp = malloc(sizeof(listint_t));
-	
-	if (!temp)
+	if (!temp || !*head)
 		return (NULL);
 
-	trav = *head;
-	for (i = 1; i < idx; i++)
+	trav = temp = edge = stag = *head;
+	while (edge != NULL)
 	{
-		trav = trav->next;
+		edge = edge->next;
+		count++;
 	}
-
-	temp->next = trav->next;
-	temp->n = n;
-	trav->next = temp;
-
+	if (idx > count)
+		return (NULL);
+	if  (idx == 0)
+	{
+		temp->n = n;
+		temp->next = *head;
+		*head = temp;
+	}
+	if (idx == 1)
+	{
+		temp->n = n;
+		temp->next = stag->next;
+		stag->next = temp;
+	}
+	if (idx > 1 && idx < count)
+	{
+		for (i = 0; i < (idx - 1); i++)
+		{
+			trav = trav->next;
+		}
+		temp->n = n;
+		temp->next = trav->next;
+		trav->next = temp;
+	}
 	return (*head);
 }
