73faa9e6efc4f381a57448d2008a9223e30bc69f
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-04 23:34:36 -0800

    made provision for when index is greater than what the number contains

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * get_bit - a function that returns the value of a bit at
+ * a given index
+ * Return: bit at that given index
+ * @n: the number to be analyzed
+ * @index: index of the bit (starting from zero)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i, mask;
+
+	i = 0;
+	mask = 1;
+	while (i < index)
+	{
+		mask = mask * 2;
+		i++;
+	}
+
+	if (mask > n)
+		return (-1);
+
+	if (mask & n)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x14-bit_manipulation/2-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int n;
+
+
+
+	    
+	n = get_bit(1024, 10);
+
+	        
+	printf("%d\n", n);
+
+		    
+	n = get_bit(98, 6);
+
+		        
+	printf("%d\n", n);
+	n = get_bit(1024, 0);
+
+	    
+	printf("%d\n", n);
+	 return (0);
+}
