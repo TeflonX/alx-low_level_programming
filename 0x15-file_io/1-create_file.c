f713066ae7c7dae11e53e777535dc5b7403d1319
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-08 22:25:02 -0800

    added condition for a null buffer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		count1 = write(fd, text_content, strlen(text_content));
 		if (count1 < 0)
