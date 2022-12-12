74cf19fdefac237a68ca644551e287b9f16bab6f
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-12 04:27:35 -0800

    betty failed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,56 +21,55 @@ void close_fd(int fd)
 	}
 }
 
+void return_error(int count, char av[i])
+{
+
 /**
  * main - copy from one file to another
  * Return: 0 (Success)
  * @ac: argument count
  * @av: double pointer to argument vector
  */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
 	int fd1, fd2, count1, count2;
 
-	char *buf;
+	char buf[1024];
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(98);
+		exit(97);
 	}
-	buf = malloc(sizeof(char) * 1024);
-	if (!buf)
-		return (1);
-
 	fd1 = open(av[1], O_RDONLY);
-	count1 = read(fd1, buf, 1024);
-	if (fd1 == -1 || count1 == -1)
+	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", av[1]);
-		free(buf);
 		exit(98);
-		}
+	}
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	while (count1 > 0)
+	if (fd2 < 0)
 	{
-		if (fd1 == -1 || count1 == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", av[1]);
-			free(buf);
-			exit(98);
-		}
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((count1 = read(fd1, buf, 1024)) > 0)
+	{
 		count2 = write(fd2, buf, count1);
 		if (fd2 < 0 || count2 < 0)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", av[2]);
-			free(buf);
+			"Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		count1 = read(fd1, buf, 1024);
-		fd2 = open(av[2], O_WRONLY, O_APPEND);
+	}
+	if (count1 < 0)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", av[1]);
+		exit(98);
 	}
 	close_fd(fd1);
 	close_fd(fd2);
