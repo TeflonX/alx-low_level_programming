8d18ffcec94c3e4ec73f1dd48b99f90bb9a73f71
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-19 00:15:12 -0800

    fixed tab and new line bug

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string -  a function that capitalizes all words of a string
+ * Return: pointer to the modified string
+ * @s: pointer to the string to be modified
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (
+		s[i] == ' ' ||
+		s[i] == '\t' ||
+		s[i] == '\n' ||
+		s[i] == ',' ||
+		s[i] == ';' ||
+		s[i] == '.' ||
+		s[i] == '?' ||
+		s[i] == '"' ||
+		s[i] == '(' ||
+		s[i] == ')' ||
+		s[i] == '{' ||
+		s[i] == '}'
+		)
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
