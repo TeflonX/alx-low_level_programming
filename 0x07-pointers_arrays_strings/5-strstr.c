a7560042cbbe8b078688fd6b1c515d7712419e13
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-25 02:20:27 -0800

    made provision for when the substring is not present in the main string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,38 @@
 #include "main.h"
 #include <stddef.h>
 #include <string.h>
-
+/**
+ * _strstr - finds the first occurence of a substring in a string
+ * @haystack: pointer to the main string
+ * @needle: pointer to the substring
+ *
+ * Return: a character pointer
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, n, flag, len1, len2;
+	int i, j, len1, len2;
 
 	len1 = strlen(haystack);
 	len2 = strlen(needle);
-	flag = 0;
-	n = len1 - len2 + 1;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < len1; i++)
 	{
-		flag = 0;
 		for (j = 0; j < len2; j++)
 		{
 			if (haystack[i + j] != needle[j])
-			{
-				flag = 1;
 				break;
-			}
+			if (j == len2)
+				break;
 		}
-		if (haystack[i + j] != needle[j])
-		{
+		if (j == len2)
 			break;
-		}
 	}
-	if (flag == 1)
+	if (i == len1 - 1)
 	{
-		return (haystack[i + j]);
+		return (NULL);
 	}
 	else
 	{
-		return (NULL);
+		return (&haystack[i]);
 	}
 }
