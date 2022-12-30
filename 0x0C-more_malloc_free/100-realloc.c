f18a4bac06794e66de34baae2e13d50354ef95a0
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-29 23:50:05 -0800

    used memcpy to transfer data from old to new memory area

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
-
-
+#include <string.h>
+/**
+ * _realloc - a function that reallocates a memory block using
+ * malloc and free
+ * @ptr: pointer to the old memory area
+ * @old_size: size in bytes, of the old memory area
+ * @new_size: size in bytes, of the new memory area
+ *
+ * Return: nothing
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i;
-
 	void *p;
 
-	ptr = malloc(old_size);
+	if (!ptr)
+	{
+		p = malloc(new_size);
+	}
 
-	p = malloc(new_size);
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		memcpy(p, ptr, old_size);
+		free(ptr);
+	}
 
-	for (i = 0; i <
+	if (old_size == new_size)
+		return (ptr);
 
+	if (!new_size && ptr)
+		return (NULL);
+
+	return (p);
 }
