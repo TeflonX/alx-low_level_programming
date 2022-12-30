613046f2ecf7519ed45b18e63e8a4774d3be138e
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-30 08:31:34 -0800

    made my code betty compliant

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * integer_check - checks if argument passed are digits
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 or 0
+ */
+int integer_check(int argc, char *argv[])
+{
+	int i, count;
+
+	count = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) > 0)
+			count++;
+	}
+	if (count == 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	return (0);
+}
+/**
+ * add_numbers - add two numbers passed as argument
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: void
+ */
+void add_numbers(int argc, char *argv[])
+{
+	int i, result;
+
+	result = (atoi(argv[1]));
+	for (i = 2; i < argc; i++)
+	{
+		result = result + (atoi(argv[i]));
+	}
+	printf("%d\n", result);
+}
+/**
  * main - function that adds two numbers.
  * Return: 0 (Success)
  * @argc: argument count.
@@ -9,60 +52,44 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, k, l, count, breaker2, result;
+	int i, j, count, breaker2;
 
 	size_t breaker1;
 
-	char *p;
+	count = integer_check(argc, argv);
+	if (count == 1)
+		return (0);
 
-	count = 0;
-	for (i = 1; i < argc; i++)
+	i = 1;
+	breaker2 = 0;
+	while (i < argc)
 	{
+		breaker1 = 0;
 		if (atoi(argv[i]) > 0)
 		{
-			count++;
-		}
-	}
-	if (count == 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	j = 1;
-	k = 0;
-	breaker1 = 0;
-	breaker2 = 0;
-	while (j < argc)
-	{
-		p = argv[j];
-		if (atoi(argv[j]) > 0)
-		{
-			while (p[k])
+			j = 0;
+			while (argv[i][j])
 			{
-				if (p[k] >= '0' && p[k] <= '9')
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
 				{
 					breaker1++;
 				}
-				k++;
+				j++;
 			}
 		}
-		if (breaker1 < strlen(argv[j]))
+		if (breaker1 < strlen(argv[i]))
 		{
 			breaker2 = 1;
 			break;
 		}
-		j++;
+		i++;
 	}
 	if (breaker2 == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	result = (atoi(argv[1]));
-	for (l = 2; l < argc; l++)
-	{
-		result = result + (atoi(argv[l]));
-	}
-	printf("%d\n", result);
+	add_numbers(argc, argv);
+
 	return (0);
 }
