86d19e6d03fc8b33a12de9fedbc425d1e292282c
TeflonX <wisdom.ikegwurur@yahoo.com>
2022-12-30 17:10:30 -0800

    a function that frees a pointer of data type, struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function that frees a struct data type
+ * @d: pointer of data type, dog_t
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
