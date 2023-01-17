4ee4dbc1025c2c56a6f17e0a67e3eb6eb9896f34
TeflonX <wisdom.ikegwurur@yahoo.com>
2023-01-17 10:41:00 -0800

    task 1: dereferenced function pointer

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+0x0F. C - Function pointers
+----------------------------
+My ALX Project on Function Pointers.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_PONTERS_H
+void print_name(char *name, void (*f)(char *));
+
+#endif
