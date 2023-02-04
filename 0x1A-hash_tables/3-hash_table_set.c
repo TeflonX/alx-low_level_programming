2f0f5e8639d7cc1cf19efbc111ba6fb748e11e4b
TeflonX <wisdom.ikegwurur@yahoo.com>
2023-02-04 03:10:45 -0800

    task 3: made provision for null pointer

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	unsigned long int index, val;
 
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 || !value || !key)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
