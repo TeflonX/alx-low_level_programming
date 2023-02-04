ffd7612f0ed138614f74f62785da91598f624fa2
TeflonX <wisdom.ikegwurur@yahoo.com>
2023-02-03 18:40:08 -0800

    task 3: modified code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,14 @@
 #include "hash_tables.h"
 #include <string.h>
 #include <stdlib.h>
+/**
+ * hash_table_set - function that adds an element to a hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with key
+ *
+ * Return: 1 or 0
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *set;
@@ -8,14 +16,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int ret_val;
 
 	ret_val = key_index((unsigned char *)key, ht->size);
-	
+
 	set = malloc((strlen(value) + 1) * sizeof(char));
 	if (!set)
 	{
-		ht->array[ret_val] = set;
-
-		(ht->array[ret_val])->value = strdup(value);
-
+		if (!(ht->array[ret_val]))
+		{
+			ht->array[ret_val] = set;
+			(ht->array[ret_val])->key = (char *)key;
+			(ht->array[ret_val])->value = strdup(value);
+		}
+		else
+		{
+			(ht->array[ret_val])->next = set;
+			set->key = (char *)key;
+			set->value = strdup(value);
+		}
 		return (1);
 	}
 	else
