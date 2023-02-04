504180bdf6384dfb0e821ab2448c51a70e1bef92
TeflonX <wisdom.ikegwurur@yahoo.com>
2023-02-04 01:30:01 -0800

    task 3: added a node to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,8 +28,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (!(ht->array[ret_val]))
 		{
 			ht->array[ret_val] = set;
-			(ht->array[ret_val])->key = set->key;
-			(ht->array[ret_val])->value = set->value;
 		}
 		else
 		{
