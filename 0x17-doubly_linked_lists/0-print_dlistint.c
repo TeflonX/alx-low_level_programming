884c4d888aed8e7d906ae6589e571f1e935b3612
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-13 18:48:25 +0000

    task 0 and 1: counted number of nodes in the linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdio.h>
 /**
- * print_dlistint - a function that prints all the elements
- * of a dlistint_t list
- * @h: pointer to the head of the doubly linked list
+ * print_dlistint - a function that prints all the elements of a dlistint_t
+ * list.
+ * @h: pointer to the head of the linked list
  *
- * Return: Number of nodes
+ * Return: count variable
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	unsigned int count;
 
 	const dlistint_t *trav;
 
-	trav = h;
 	count = 0;
+	trav = h;
 	while (trav)
 	{
 		printf("%d\n", trav->n);
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,8 @@
 #include "lists.h"
-/**
- * dlistint_len - a function that returns the number of elements
- * in a linked dlistint_t list
- * @h: pointer to the head of the doubly linked list
- *
- * Return: number of node
- */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
+	unsigned int count;
 
 	const dlistint_t *trav;
 
@@ -19,6 +13,6 @@ size_t dlistint_len(const dlistint_t *h)
 		count++;
 		trav = trav->next;
 	}
-
 	return (count);
+
 }
